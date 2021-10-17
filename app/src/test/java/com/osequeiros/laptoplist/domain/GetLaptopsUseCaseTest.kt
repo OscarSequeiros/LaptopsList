66f87d933bc03140792ccb1040ad0c6aa0e1c510
Oscar Sequeiros <oscar.sequeiros.serrano@gmail.com>
2021-10-17 15:17:28 -0500

    build: Using kotlin 1.5.31

---
+++ app/src/test/java/com/osequeiros/laptoplist/domain/GetLaptopsUseCaseTest.kt
@@ -0,0 +1,12 @@
+package com.osequeiros.laptoplist.domain
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class GetLaptopsUseCaseTest {
+
+    @Test
+    operator fun invoke() {
+    }
+}
---
+++ app/src/test/java/com/osequeiros/laptoplist/factory/LaptopFactory.kt
@@ -0,0 +1,4 @@
+package com.osequeiros.laptoplist.factory
+
+class LaptopFactory {
+}
